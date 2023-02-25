90b2a62192f47a587bad5841229c509b1c3f9f8f
vitaliy <vgork2805@gmail.com>
2023-02-25 12:28:58 +0700

    Написан код

---
+++ program/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите количество элементов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    arrayStrings[i] = element;
+}
+
+string[] arrayFinal = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (arrayStrings[j].Length <= len)
+    {
+        arrayFinal[pos] = arrayStrings[j];
+        pos++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayFinal);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
